00fcb0a85fb58d53d1bb176ce8f224e1c660ff63
David <davidroussel171113@outlook.fr>
2024-05-01 10:48:21 +0200

    Modification nom de methode ajout d'une boucle while

--- Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -13,20 +13,24 @@ public class WriteSymptomDataToFile implements ISymptomWriter {
     public WriteSymptomDataToFile(String filepath) {this.filepath = filepath;}
 
     @Override
-    public Map<String, Integer> postSymptoms() {
+    public Map<String, Integer> writeSymptoms() {
         ArrayList<String> result = new ArrayList<>();
 
         if (filepath != null) {
             try {
                 BufferedWriter writer = new BufferedWriter(new FileWriter(filepath, false));
+                String line = writer.toString();
 
                 writer.write("Liste des symptoms");
-
+                writer.newLine();
+                while (line != null) {
+                    result.add(line);
+                }
                 writer.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-        return result;
+        return (Map<String, Integer>) result;
     }
 }
