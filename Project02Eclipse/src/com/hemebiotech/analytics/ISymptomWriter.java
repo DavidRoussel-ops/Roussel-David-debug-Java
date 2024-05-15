9503685c421ebc983769bba1ede10d54e7cb7422
David <davidroussel171113@outlook.fr>
2024-05-15 16:09:13 +0200

    Modification du nom suite à la modification apporté à la méthode

--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomWriter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomWriter.java
@@ -5,5 +5,5 @@ import java.util.Map;
 public interface ISymptomWriter {
 
     // Méthode de l'interface
-    public void writeSymptoms(Map<String, Integer> symptoms);
+    void WriteSymptoms(Map<String, Integer> symptoms);
 }
