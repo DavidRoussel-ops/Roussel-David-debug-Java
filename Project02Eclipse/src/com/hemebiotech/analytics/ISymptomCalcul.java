8a38c9cb25a02adfbba94d441fe6e276f9b0b013
David <davidroussel171113@outlook.fr>
2024-05-15 16:11:29 +0200

    Mise en place d'une interface pour la méthode de calcul d'occurence.

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomCalcul.java
@@ -0,0 +1,9 @@
+package com.hemebiotech.analytics;
+
+import java.util.List;
+import java.util.Map;
+
+public interface ISymptomCalcul {
+
+    Map<String, Integer> CalculSymptoms(List<String> symptoms);
+}
