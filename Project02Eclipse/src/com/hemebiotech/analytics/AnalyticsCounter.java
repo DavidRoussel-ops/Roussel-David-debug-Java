4e579e85f0949f82e9ee517750b272e173402606
David <davidroussel171113@outlook.fr>
2024-05-16 22:50:28 +0200

    Mise en place de l'appel des méthodes dans le main

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,38 +1,24 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;
-	private static int rashCount = 0;
-	private static int pupilCount = 0;
-	private final ISymptomReader reader;
-	private final ISymptomWriter writer;
-
-	// Constructeur pour les class ISymptomReader et ISymptomWriter
-	public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer) {
-		this.reader = reader;
-		this.writer = writer;
-	}
 	
 	public static void main(String[] args) throws Exception {
-
-		// Méthode getSymptoms pour récupérer la liste des entrées dans le fichier
-		public List<String> getSymptoms("symptoms.txt") {}
-
-		// Méthode countSymptoms qui va conter les occurences de chaque symptoms existant dans la liste
-		public Map<String, Integer> countSymptoms(List<String> symptoms) {}
-
-		// Méthode sortSymptoms qui va trié la liste de symptoms et d'occurences par ordre alphabétique
-		public Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms) {}
-
-		// Méthode writeSymptoms qui écrit le résultat dans le fichier de sortie en utilisant l'instance de ISymptomWriter
-		public void writeSymptoms(Map<String, Integer> symptoms) {}
-
-	}
+		try {
+			List<String> result = new ArrayList<>();
+			Map<String, Integer> map = new TreeMap<>();
+			ReadSymptomDataFromFile readSymptomDataFromFile = new ReadSymptomDataFromFile("C:/Roussel-David-debug-Java/Project02Eclipse/symptoms.txt");
+			result = readSymptomDataFromFile.GetSymptoms();
+			CalculOccurences calculOccurences = new CalculOccurences();
+			map = calculOccurences.CalculSymptoms(result);
+			WriteSymptomDataToFile writeSymptomDataToFile = new WriteSymptomDataToFile("result.out");
+			writeSymptomDataToFile.WriteSymptoms(map);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
 }
