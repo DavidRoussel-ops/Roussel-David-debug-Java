c4ebdf24592cdd9eaf097bbab80db0f4b9ae40d3
David <davidroussel171113@outlook.fr>
2024-05-20 15:01:05 +0200

    Ajout de commentaires

--- Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -15,23 +15,24 @@ public class WriteSymptomDataToFile implements ISymptomWriter {
 
         if (filepath != null) {
             try {
-                // Création d'un fileWriter pour écrire dans le fichier
+                // Création d'un fileWriter pour écrire dans le fichier.
                 FileWriter fileWriter = new FileWriter("result.out", false);
 
-                // Création d'un bufferedWriter qui utilise le fileWriter
+                // Création d'un bufferedWriter qui utilise le fileWriter.
                 BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
 
-                // Ecriture des symptoms
+                // Ecriture des symptoms.
                 symptoms.forEach((key, value) -> {
                     try {
-                        bufferedWriter.write(key + ":" + value);
+                        bufferedWriter.write(key + ":" + value + "\n");
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
                 });
-
+                // Arret du BufferedWriter.
                 bufferedWriter.close();
             } catch (IOException e) {
+                // Gestion de l'exception qui affichera une trace dans le fichier log
                 e.printStackTrace();
             }
         }
