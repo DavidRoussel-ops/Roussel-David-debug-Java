250fe877a77c160020111ba3cebba5b6f3917713
David <davidroussel171113@outlook.fr>
2024-05-20 14:59:38 +0200

    Ajout de commentaires

--- Project02Eclipse/src/com/hemebiotech/analytics/CalculOccurences.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/CalculOccurences.java
@@ -8,14 +8,15 @@ public class CalculOccurences implements ISymptomCalcul {
     @Override
     public Map<String, Integer> CalculSymptoms(List<String> symptoms) {
 
-        //Création d'une nouvel map
+        //Création d'une nouvel map.
         Map<String, Integer> map = new TreeMap<>();
 
         try {
-
+            // Utilisation d'une boucle forEach qui va récupérer chaque occurences dans le fichier et ajouté +1 si déjà trouvé la valeur par défault sera 0.
             symptoms.forEach(symptom -> map.put(symptom, map.getOrDefault(symptom, 0) + 1));
 
         } catch (Exception e) {
+            // Gestion de l'exception qui affichera une trace dans le fichier log.
             e.printStackTrace();
         }
         return map;
